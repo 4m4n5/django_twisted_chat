90e9002ed04e6081541d58771fef87a8dabe229b
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-12-24 15:44:49 +0530

    Created app-chat, defined models

---
+++ chat/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.name
+    
+from django.contrib import admin
+from chat.models import ChatRoom
+
+admin.site.register(ChatRoom)
